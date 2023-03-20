3857de499c78874e6e729d788165b49dd7282a9f
Chris Stone <94856281+atomniketh@users.noreply.github.com>
2023-03-20 16:07:45 -0600

    Update SendFeedback.jsx

--- src/components/SendFeedback.jsx
+++ src/components/SendFeedback.jsx
@@ -6,14 +6,13 @@ import { generateProof } from "@semaphore-protocol/proof";
 import { ethers, utils } from "ethers"
 import FeedbackContractABI from '../abi/Feedback.json';
 
-const feedbackAddress = "0x0C339f45aB084F48C60F82Fecb1844C72a6CcaDa";
-const groupID = "444";
-let provider = new ethers.providers.Web3Provider(window.ethereum, "any");
-
 // console.log("Identity: " + localStorage.getItem('myIdentity'));
 // console.log("Commitment: " + localStorage.getItem('myCommitment'));
 
 async function getProof() {
+  const feedbackAddress = "0x0C339f45aB084F48C60F82Fecb1844C72a6CcaDa";
+  const groupID = "444";
+  let provider = new ethers.providers.Web3Provider(window.ethereum, "any");
   const group = new Group(groupID);
   //const externalNullifier = utils.formatBytes32String("Topic");
   const signal = utils.formatBytes32String("Hello world");
@@ -21,13 +20,29 @@ async function getProof() {
   // const feedbackBytes32 = formatBytes32String(textareaValue);
   const identity2 = new Identity();
   group.addMember(identity2.commitment);
-  console.log("Identity: " + identity2);
-  const externalNullifier = group.root;
-  const fullProof = await generateProof(identity2, group, externalNullifier, signal);
+
+
+  const userName = "0x636d73746f6e6500000000000000000000000000000000000000000000000000";
 
   await provider.send("eth_requestAccounts", []);
   const signer = provider.getSigner();
+  //let userAddress = await signer.getAddress();
   let contract = new ethers.Contract(feedbackAddress, FeedbackContractABI.abi, signer);
+  const tx = await contract.joinGroup(identity2.commitment, userName);
+  console.log(`Transaction hash: https://goerli.etherscan.io/tx/${tx.hash}`);
+  const receipt = await tx.wait();
+  console.log(`Transaction confirmed in block ${receipt.blockNumber}`);
+  console.log(`Gas used: ${receipt.gasUsed.toString()}`);
+
+
+
+
+  const externalNullifier = group.root;
+  const fullProof = await generateProof(identity2, group, externalNullifier, signal);
+
+  // await provider.send("eth_requestAccounts", []);
+  // const signer = provider.getSigner();
+  // let contract = new ethers.Contract(feedbackAddress, FeedbackContractABI.abi, signer);
   const idNullifier = identity2.nullifier;
   let proofString = fullProof.proof;
   console.log("Length: " + proofString.length);
@@ -41,18 +56,22 @@ async function getProof() {
   console.log("idNullifier: " + idNullifier);
   console.log("proofString: " + proofString);
 
-  const gasPrice = await signer.getGasPrice();
-  console.log("gasPrice: " + gasPrice);
-  // const gasLimit = await contract.estimateGas.greet(signal, externalNullifier, idNullifier, proofString);
-  // console.log("gasLimit: " + gasLimit);
-  let nonce = 100;
-  const txGreet = await contract.greet(signal, externalNullifier, idNullifier, proofString, { gasLimit: 100000, nonce: nonce || undefined, });
+  // const gasPrice = await signer.getGasPrice();
+  // console.log("gasPrice: " + gasPrice);
+
+  let nonce = await signer.getTransactionCount();
+
+  // const gasEstimated = await contract.estimateGas.greet(signal, externalNullifier, idNullifier, proofString, { gasLimit: 1000000, nonce: nonce || undefined, });
+  // console.log("gasEstimated: " + gasEstimated);
+
+
+  const txGreet = await contract.greet(signal, externalNullifier, idNullifier, proofString, { gasLimit: 1000000, nonce: nonce || undefined, });
   console.log(`Transaction hash: https://goerli.etherscan.io/tx/${txGreet.hash}`);
   const receiptGreet = await txGreet.wait();
   console.log(`Transaction confirmed in block ${receiptGreet.blockNumber}`);
   console.log(`Gas used: ${receiptGreet.gasUsed.toString()}`);
  }
-getProof();
+// getProof();
 
 const Feedback = () => {
 
@@ -64,7 +83,7 @@ const Feedback = () => {
       <p>Anonymous Message:</p>
       <p><textarea id="messageTxt" name="messageTxt" rows="4" cols="50">
       </textarea></p>
-      <p><button type="button" onClick={generateProof}></button></p>
+      <p><button type="button" onClick={ getProof }>Click here to send the Signal</button></p>
     </div>
   );
 };
