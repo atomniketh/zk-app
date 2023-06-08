9a9fc8140016fbf74c392bb1c95cf90ffd0be699
Chris Stone <atomnik.eth@icloud.com>
2023-06-08 11:39:59 -0600

    Edits

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-import './App.css';
-import './w3.css';
+import "./App.css";
+import "./w3.css";
 // import './zkApp.css';
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import Messages from "./components/Messages";
 import AllGroups from "./components/AllGroups";
 import CreateGroup from "./components/CreateGroup";
@@ -14,42 +19,47 @@ import SendMessage from "./components/SendMessage";
 import SendFile from "./components/SendFile";
 import MyGroups from "./components/MyGroups";
 import MessageAdmin from "./components/MessageAdmin";
-
+import Administration from "./components/Administration";
 
 // import AddUser from "./components/AddUser";
 function App() {
   return (
     <Router>
-    <>
-      {/* This is the alias of BrowserRouter i.e. Router */}
-      <Routes>
+      <>
+        {/* This is the alias of BrowserRouter i.e. Router */}
+        <Routes>
           {/* This route is for Identities component 
           with exact path "/", in component props 
           we passes the imported component */}
           {/* <Route path="/" element={<Home />} /> */}
           <Route index element={<MyGroups />} />
-          <Route path='*' element={<AllGroups />} />
-          <Route path='/Messages' element={<Messages/>} />
-          <Route path='/AllGroups' element={<AllGroups/>} />
-          <Route path='/CreateGroup' element={<CreateGroup/>} />
-          <Route path='/UpdateGroupName' element={<UpdateGroupName/>} />
-          <Route path='/UpdateEditor' element={<UpdateEditor/>} />
-          <Route path='/UpdateVerifierContract' element={<UpdateVerifierContract/>} />
-          <Route path='/CreateIdentity' element={<CreateIdentity/>} />
-          <Route path='/AddMember' element={<AddMember/>} />
-          <Route path='/SendMessage' element={<SendMessage/>} />
-          <Route path='/SendFile' element={<SendFile/>} />
-          <Route path='/MyGroups' element={<MyGroups/>} />
-          <Route path='/MessageAdmin' element={<MessageAdmin/>} />
+          <Route path="*" element={<AllGroups />} />
+          <Route path="/Messages" element={<Messages />} />
+          <Route path="/AllGroups" element={<AllGroups />} />
+          <Route path="/CreateGroup" element={<CreateGroup />} />
+          <Route path="/UpdateGroupName" element={<UpdateGroupName />} />
+          <Route path="/UpdateEditor" element={<UpdateEditor />} />
+          <Route
+            path="/UpdateVerifierContract"
+            element={<UpdateVerifierContract />}
+          />
+          <Route path="/CreateIdentity" element={<CreateIdentity />} />
+          <Route path="/AddMember" element={<AddMember />} />
+          <Route path="/SendMessage" element={<SendMessage />} />
+          <Route path="/SendFile" element={<SendFile />} />
+          <Route path="/MyGroups" element={<MyGroups />} />
+          <Route path="/MessageAdmin" element={<MessageAdmin />} />
+          <Route path="/Administration" element={<Administration />} />
+
           {/* If any route mismatches the upper 
           route endpoints then, redirect triggers 
           and redirects app to home component with to="/" */}
           {/* <Redirect to="/" /> */}
           <Route path="/" element={<Navigate replace to="/home" />} />
-      </Routes>
-    </>
+        </Routes>
+      </>
     </Router>
   );
 }
-  
+
 export default App;
---
+++ src/components/Administration.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+// import { Link } from "react-router-dom";
+import { ethers } from "ethers";
+import { sidebar } from "./Sidebar";
+import SemaphoreCommunitiesABI from "../abi/SemaphoreCommunities.json";
+
+const semaphoreCommunitiesAddress = process.env.REACT_APP_WBCONTRACT;
+
+async function updateVerifierContract() {
+  const newVerifierContract = document.getElementById(
+    "newVerifierContract"
+  ).value;
+
+  const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
+  await provider.send("eth_requestAccounts", []);
+  const signer = provider.getSigner();
+  const contract = new ethers.Contract(
+    semaphoreCommunitiesAddress,
+    SemaphoreCommunitiesABI.abi,
+    signer
+  );
+  const tx = await contract.updateVerifierContract(newVerifierContract);
+  console.log("Success!");
+  console.log(`Transaction hash: https://goerli.etherscan.io/tx/${tx.hash}`);
+  document.getElementById("updateVerifierAddressForm").reset();
+  const receipt = await tx.wait();
+  console.log(`Transaction confirmed in block ${receipt.blockNumber}`);
+  console.log(`Gas used: ${receipt.gasUsed.toString()}`);
+}
+
+const Administration = () => (
+  <div className="w3-container" style={{ marginLeft: "0", paddingLeft: "0" }}>
+    {sidebar}
+
+    <div className="w3-main" style={{ marginLeft: "250px" }}>
+      <h1>Update Semaphore Contract</h1>
+      <br />
+      {/* <Link to="/AllGroups">All Groups</Link>
+      <p></p> */}
+      <form id="updateVerifierAddressForm">
+        <label htmlFor="newVerifierContract">
+          New Semaphore Contract Address:
+        </label>{" "}
+        &nbsp;
+        <input
+          type="text"
+          id="newVerifierContract"
+          name="newVerifierContract"
+          size="48"
+        />
+        <p></p>
+      </form>
+      <p>
+        <button type="button" onClick={updateVerifierContract} className="w3-button w3-block w3-section w3-black w3-ripple w3-padding" >
+          Click here to update Semaphore Contract.
+        </button>
+      </p>
+    </div>
+  </div>
+);
+
+export default Administration;
